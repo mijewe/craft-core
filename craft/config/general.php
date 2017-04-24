8db630c37a10504781ab8d1bb24615886f8dca6f
Michael Westwood <michael@du.st>
2017-04-24 13:19:48 +0100

    Now using Craft Multi Environment for multi environments.

--- craft/config/general.php
+++ craft/config/general.php
@@ -1,65 +1,68 @@
 <?php
 
 /**
- * General Configuration
- *
- * All of your system's general configuration settings go in here.
- * You can see a list of the default settings in craft/app/etc/config/defaults/general.php
- */
+* General Configuration
+*
+* All of your system's general configuration settings go in here.
+* You can see a list of the default settings in craft/app/etc/config/defaults/general.php
+*/
 
+// $_ENV constants are loaded by craft-multi-environment from .env.php via public/index.php
 return array(
-    '*' => array(
-        'defaultFilePermissions' => 0664,
-        'defaultFolderPermissions' => 0775,
-        'addTrailingSlashesToUrls' => true,
-        'limitAutoSlugsToAscii' => true,
-        'useEmailAsUsername' => true,
-        'convertFilenamesToAscii' => true,
-        'defaultImageQuality' => 100,
-        'maxUploadFileSize' => 40000000,
-        'omitScriptNameInUrls' => true,
-		'overridePhpSessionLocation' => true,
-        // 'defaultCookieDomain' => '.example.com',
-        'defaultWeekStartDay' => 1,
-        'preserveImageColorProfiles' => true,
-        'gaCode' => false,
-        'locales' => array(
-            'en_gb' => array(
-                'languageCode' => 'en',
-                'niceLanguage' => 'English',
-                'dateFormat' => 'jS F Y'
-            )
-        ),
-        'mainLocale' => 'en_gb'
-    ),
 
-    // Settings when developing locally.
-    'core.dev' => array(
-        'devMode' => true,
-        'siteUrl' => array(
-            'en_gb' => 'http://core.dev/'
-        ),
-        'rootUrl' => 'http://core.dev',
-        'trackUsers' => false,
-        'omitScriptNameInUrls' => false,
-        'defaultCookieDomain' => ''
+  // All environments
+  '*' => array(
+    'defaultFilePermissions' => 0664,
+    'defaultFolderPermissions' => 0775,
+    'addTrailingSlashesToUrls' => true,
+    'limitAutoSlugsToAscii' => true,
+    'useEmailAsUsername' => true,
+    'convertFilenamesToAscii' => true,
+    'defaultImageQuality' => 82,
+    'maxUploadFileSize' => 40000000,
+    'omitScriptNameInUrls' => true,
+    'overridePhpSessionLocation' => true,
+    // 'defaultCookieDomain' => '.example.com',
+    'defaultWeekStartDay' => 1,
+    'preserveImageColorProfiles' => true,
+    'usePathInfo' => true,
+    'cacheDuration' => false,
+    'siteUrl' => getenv('CRAFTENV_SITE_URL'),
+    'craftEnv' => CRAFT_ENVIRONMENT,
+    'environmentVariables' => array(
+      'baseUrl'  => getenv('CRAFTENV_BASE_URL'),
+      'basePath' => getenv('CRAFTENV_BASE_PATH'),
     ),
+    'rootUrl' => trim( getenv('CRAFTENV_BASE_URL'), '/' ),
 
-    // Settings when on the live site.
-    'example.com' => array(
-        'siteUrl' => array(
-            'en_gb' => 'http://example.com/'
-        ),
-        'rootUrl' => 'http://example.com',
-        'trackUsers' => true
+    'gaCode' => false,
+    'locales' => array(
+      'en_gb' => array(
+        'languageCode' => 'en',
+        'niceLanguage' => 'English',
+        'dateFormat' => 'jS F Y'
+      )
     ),
+    'mainLocale' => 'en_gb'
+  ),
 
-    // Settings when on the staging site.
-    'staging.example.com' => array(
-        'siteUrl' => array(
-            'en_gb' => 'http://staging.example.com/'
-        ),
-        'rootUrl' => 'http://staging.example.com',
-        'trackUsers' => false
-    )
+  // Live (production) environment
+  'live'  => array(
+    'devMode' => false,
+    'trackUsers' => true,
+  ),
+
+  // Staging (pre-production) environment
+  'staging'  => array(
+    'devMode' => false,
+    'trackUsers' => false,
+  ),
+
+  // Local (development) environment
+  'local'  => array(
+    'devMode' => true,
+    'trackUsers' => false,
+    'omitScriptNameInUrls' => false,
+    'defaultCookieDomain' => ''
+  ),
 );
