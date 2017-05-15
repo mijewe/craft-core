7161cbfea96340f10506a0738519781a1ab88bdc
Michael Westwood <michael@du.st>
2017-05-15 15:38:31 +0100

    Now copying and uglifying JS snippets on init.

--- gulp/init.js
+++ gulp/init.js
@@ -2,22 +2,52 @@ var gulp = require('gulp');
 var gulpCopy = require('gulp-copy');
 var mkdirp = require('mkdirp');
 var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
 
+// copies the Craft app directory to the right place.
 gulp.task("init:app", function () {
     return gulp.src("./bower_components/craft/craft/app/**/*")
-    .pipe(gulpCopy('./craft', { prefix: 2 }))
+    .pipe(gulpCopy('./craft', { prefix: 3 }))
 });
 
+// creates an empty storage folder
 gulp.task("init:storage", function () {
   mkdirp('./craft/storage', function (err) {
     if (err) console.error(err)
   });
 });
 
+// copies our example env config to a useful place.
 gulp.task("init:env", function () {
   return gulp.src("example.env.php")
   .pipe(rename('.env.php'))
   .pipe(gulp.dest("."));
 });
 
-gulp.task('init', ['init:app', 'init:storage', 'init:env', 'plugins']);
+// copies the loadCSS snippet to templates.
+gulp.task("init:loadcss", function () {
+  return gulp.src("bower_components/loadcss/loadCSS.js")
+  .pipe(uglify())
+  .pipe(rename('loadCSS.js'))
+  .pipe(gulp.dest("craft/templates/_partials/snippets/"));
+});
+
+// copies the onLoadCSS snippet to templates.
+gulp.task("init:onloadcss", function () {
+  return gulp.src("bower_components/loadcss/onloadcss.js")
+  .pipe(uglify())
+  .pipe(rename('onloadcss.js'))
+  .pipe(gulp.dest("craft/templates/_partials/snippets/"));
+});
+
+// copies the fontfaceobserver snippet to templates.
+gulp.task("init:fontfaceobserver", function () {
+  return gulp.src("bower_components/fontfaceobserver/fontfaceobserver.js")
+  .pipe(uglify())
+  .pipe(rename('fontfaceobserver.js'))
+  .pipe(gulp.dest("craft/templates/_partials/snippets/"));
+});
+
+gulp.task('init:js', ['init:loadcss', 'init:onloadcss', 'init:fontfaceobserver']);
+
+gulp.task('init', ['init:app', 'init:storage', 'init:env', 'plugins', 'init:js']);
