42bf767f47bd91bacf7b9b1c69989f2e3aeec24f
Michael Westwood <michael@du.st>
2017-05-15 15:08:09 +0100

    Moved craft app dest to the proper place.

--- gulp/init.js
+++ gulp/init.js
@@ -5,11 +5,11 @@ var rename = require("gulp-rename");
 
 gulp.task("init:app", function () {
     return gulp.src("./bower_components/craft/craft/app/**/*")
-    .pipe(gulpCopy('./test', { prefix: 2 }))
+    .pipe(gulpCopy('./craft', { prefix: 2 }))
 });
 
 gulp.task("init:storage", function () {
-  mkdirp('./test/craft/storage', function (err) {
+  mkdirp('./craft/storage', function (err) {
     if (err) console.error(err)
   });
 });
