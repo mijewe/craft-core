a6950339c57ca9e410e947a03d9781222a92cb47
Michael Westwood <michael@du.st>
2017-06-07 12:52:05 +0200

    Added in grunt criticalcss, removed all other grunt stuff.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,71 +2,10 @@ module.exports = function(grunt) {
 
 	// Project configuration
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		// Global configuration variables
-		dirs: {
-			src: 'src',
-			dest: 'public',
-			css_build_folder: '/assets/css/',
-			js_build_folder: '/assets/js/',
-			img_build_folder: '/assets/img/'
-		}
+		pkg: grunt.file.readJSON('package.json')
 	});
-
-	// Load config-less tasks
-	grunt.loadNpmTasks('grunt-newer');
 
 	// Load per-task config from separate files.
 	grunt.loadTasks('grunt');
-
-	// Default task(s)
-	grunt.registerTask('default', '', [
-		// do nothing.
-	]);
-
-	grunt.registerTask('init', 'Install Craft.', [
-		'copy:craftone',
-		'copy:crafttwo',
-		'copy:craftthree'
-	]);
-
-	grunt.registerTask('styles', 'Do your styles.', [
-		'sass:dist',
-		'autoprefixer'
-	]);
-
-	grunt.registerTask('scripts', 'Do your scripts.', [
-		'concat',
-		'uglify'
-	]);
-
-	grunt.registerTask('images', 'Do your images.', [
-		'clean',
-		'svgmin'
-	]);
-
-	grunt.registerTask('plugins', 'Install plugins.', [
-		'copy:pluginanchor',
-		'copy:plugincachey',
-		'copy:pluginemailwrap',
-		'copy:pluginrefreshstring',
-		'copy:plugintypogrify',
-		'copy:plugincontactform',
-		'copy:pluginredirectmanager',
-		'copy:pluginsproutfields',
-		'copy:pluginjsontransforms',
-		'copy:pluginjsonreader',
-		'copy:pluginfieldmanager'
-	]);
-
-	grunt.registerTask('build', 'Rebuild everything.', [
-		'plugins',
-		'copy:fonts',
-		'grunticon',
-		'styles',
-		'scripts',
-		'images',
-		'favicons'
-	]);
 
 };
