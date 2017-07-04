3b1caacd4de28127519d9eb0c8788dbec7cdfe1f
Michael Westwood <michael@du.st>
2017-07-04 17:14:48 +0300

    New which breakpoint method; more futureproof.

--- src/assets/js/core.js
+++ src/assets/js/core.js
@@ -22,24 +22,9 @@ Array.prototype.contains = function(obj) {
 /*
 An easy way to check which breakpoint we're currently in.
 */
+
 function whichBreakpoint() {
-	var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-
-	var breakpoint = 'mouse';
-
-	if (width > 1040) {
-		breakpoint = 'bear';
-	} else if (width > 800) {
-		breakpoint = 'wolf';
-	} else if (width > 600) {
-		breakpoint = 'fox';
-	} else if (width > 480) {
-		breakpoint = 'rabbit';
-	} else {
-		breakpoint = 'mouse';
-	}
-
-	return breakpoint;
+	return window.getComputedStyle(document.querySelector('.which-breakpoint'), ':before').getPropertyValue('content').replace(/\"/g, '');
 }
 
 
