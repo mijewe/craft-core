42e02175ddf5428416dc3f21684037fdc3cbfa90
Michael Westwood <michael@du.st>
2017-07-04 14:50:31 +0300

    Added commented out foreach loop for ref.

---
+++ src/assets/js/main.js
@@ -0,0 +1,14 @@
+/*
+ready(function() {
+
+});
+*/
+
+/*
+Basic foreach loop.
+===================
+var items = document.querySelectorAll('');
+for (var i = 0; i < items.length; i++) {
+  var item = items[i];
+}
+*/
