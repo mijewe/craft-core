8a903cf84865e7b033fae554810ecb751435d5e8
Michael Westwood <michaelwestwood@Michaels-MacBook-Pro.local>
2017-11-12 09:32:37 +0000

    Plugins are now node_modules

--- gulp/plugins.js
+++ gulp/plugins.js
@@ -2,52 +2,52 @@ var gulp = require('gulp');
 var gulpCopy = require('gulp-copy');
 
 gulp.task("plugin:anchor", function () {
-    return gulp.src("./bower_components/craft-anchor/anchor/**/*")
+    return gulp.src("./node_modules/craft-anchor/anchor/**/*")
     .pipe(gulpCopy('./craft/plugins/', { prefix: 2 }))
 });
 
 gulp.task("plugin:cachey", function () {
-    return gulp.src("./bower_components/craft-cachey/cachey/**/*")
+    return gulp.src("./node_modules/craft-cachey/cachey/**/*")
     .pipe(gulpCopy('./craft/plugins/', { prefix: 2 }))
 });
 
 gulp.task("plugin:emailwrap", function () {
-    return gulp.src("./bower_components/craft-emailwrap/emailwrap/**/*")
+    return gulp.src("./node_modules/craft-emailwrap/emailwrap/**/*")
     .pipe(gulpCopy('./craft/plugins/', { prefix: 2 }))
 });
 
 gulp.task("plugin:refreshstring", function () {
-    return gulp.src("./bower_components/craft-refreshstring/refreshstring/**/*")
+    return gulp.src("./node_modules/craft-refreshstring/refreshstring/**/*")
     .pipe(gulpCopy('./craft/plugins/', { prefix: 2 }))
 });
 
 gulp.task("plugin:typogrify", function () {
-    return gulp.src("./bower_components/typogrify/typogrify/**/*")
+    return gulp.src("./node_modules/craft-typogrify/typogrify/**/*")
     .pipe(gulpCopy('./craft/plugins/', { prefix: 2 }))
 });
 
 gulp.task("plugin:contactform", function () {
-    return gulp.src("./bower_components/contactform/contactform/**/*")
+    return gulp.src("./node_modules/ContactForm/contactform/**/*")
     .pipe(gulpCopy('./craft/plugins/', { prefix: 2 }))
 });
 
 gulp.task("plugin:redirectmanager", function () {
-    return gulp.src("./bower_components/redirectmanager/redirectmanager/**/*")
+    return gulp.src("./node_modules/Craft-Plugin--Redirect-Manager/redirectmanager/**/*")
     .pipe(gulpCopy('./craft/plugins/', { prefix: 2 }))
 });
 
 gulp.task("plugin:jsontransforms", function () {
-    return gulp.src("./bower_components/craft-jsontransforms/jsontransforms/**/*")
+    return gulp.src("./node_modules/craft-jsontransforms/jsontransforms/**/*")
     .pipe(gulpCopy('./craft/plugins/', { prefix: 2 }))
 });
 
 gulp.task("plugin:jsonreader", function () {
-    return gulp.src("./bower_components/craft-jsonreader/jsonreader/**/*")
+    return gulp.src("./node_modules/craft-jsonreader/jsonreader/**/*")
     .pipe(gulpCopy('./craft/plugins/', { prefix: 2 }))
 });
 
 gulp.task("plugin:imager", function () {
-    return gulp.src("./bower_components/imager-craft/imager/**/*")
+    return gulp.src("./node_modules/Imager-Craft/imager/**/*")
     .pipe(gulpCopy('./craft/plugins/', { prefix: 2 }))
 });
 
